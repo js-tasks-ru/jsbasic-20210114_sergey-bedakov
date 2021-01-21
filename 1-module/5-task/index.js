a6350b55c9df92050f99b837082418174e64ae1e
Rock <rogue_87@mail.ru>
2021-01-21 22:49:05 +0300

    решение задачи 1.5 копипаста, т.к не осилил(

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,14 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+  let strLength = str.length;
+  let isExceedMaxlength = strLength > maxlength;
+
+  if (isExceedMaxlength) {
+    let shortenStr = str.slice(0, maxlength - 1);
+
+    return `${shortenStr}…`;
+  }
+
+  return str;
 }
