495861cb74ed1806ce28017ac2240dc456768f0a
Rock <rogue_87@mail.ru>
2021-01-22 19:58:41 +0300

    удалил console.log

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -7,7 +7,6 @@ function checkSpam(str) {
   // ваш код...
   let lowerCaseString = str.toLowerCase();
   if ((lowerCaseString.indexOf('1xbet') >= 0) || (lowerCaseString.indexOf('xxx') >= 0)) {
-    console.log(lowerCaseString);
     return true;
   } 
   return false;
