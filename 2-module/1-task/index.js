918e45102863b2e7fa5ede34373b28bcdac6236b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-02-14 13:42:41 +0300

    Added solution for task: Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,8 +1,13 @@
-/**
- * Складываем зарплаты
- * @param {Object} salaries - объект зарплат
- * @returns {Number}
- */
 function sumSalary(salaries) {
-  // ваш код...
+  let sumOfSalaries = 0;
+
+  for (let key in salaries) {
+    let isSalary = typeof salaries[key] === 'number' && Number.isFinite(salaries[key]);
+
+    if (isSalary) {
+      sumOfSalaries += salaries[key];
+    }
+  }
+
+  return sumOfSalaries;
 }
